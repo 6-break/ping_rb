72a4eefb64b97a120324bb42787e0b9f7f43654c
ao <regios0710@live.jp>
2016-02-23 01:06:47 +0900

    add run method to RunCommand class

--- CommandInput2.rb
+++ CommandInput2.rb
@@ -27,28 +27,31 @@ class RunCommand
   # IPアドレスの登録、実行  
   def addipaddr
   # IPアドレス情報のインスタンス
-  ip_info = IpInfo.new("")
+  @ip_info = IpInfo.new("")
   
   # 登録するIPアドレス入力
    print "\n"
    print "宛先IPアドレス: "
-   ip_info.dst_ip = gets.chomp.to_s 
-   
+   @ip_info.dst_ip = gets.chomp.to_s 
+  end
+
   # 入力されたIPアドレスをコマンドに代入して実行
-  exec("ping " + ip_info.dst_ip)
+  def run
+  exec("ping " + @ip_info.dst_ip)
    begin
    rescue
     puts "error"
    end
   end
+  
 end
-
 
 # インスタンスnew
 run_command = RunCommand.new
 
-# IPアドレス入力画面処理, コマンド実行 
+# IPアドレス入力画面処理
 run_command.addipaddr
 
-
+# コマンド実行
+run_command.run
 
