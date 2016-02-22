e8273723544dd920bbb0f0cd699c51c46ed51fb7
ao <regios0710@live.jp>
2016-02-22 22:45:48 +0900

    first commit

---
+++ CommandInput2.rb
@@ -0,0 +1,54 @@
+
+# IPアドレス情報クラス
+class IpInfo
+  # 初期化
+  def initialize(dst_ip)
+  @dst_ip = dst_ip
+  end
+
+  # アクセサ
+  attr_accessor :dst_ip
+
+  # 文字表現を返すメソッド
+  def to_s
+   "#{@dst_ip}"
+  end
+  
+  # コマンド出力フォーマット
+  def toFormattedCommand
+    "#{@dst_ip}"
+  end
+  
+ end
+
+# コマンド実行クラス
+class RunCommand
+  
+  # IPアドレスの登録、実行  
+  def addipaddr
+  # IPアドレス情報のインスタンス
+  ip_info = IpInfo.new("")
+  
+  # 登録するIPアドレス入力
+   print "\n"
+   print "宛先IPアドレス: "
+   ip_info.dst_ip = gets.chomp.to_s 
+   
+  # 入力されたIPアドレスをコマンドに代入して実行
+  exec("ping " + ip_info.dst_ip)
+   begin
+   rescue
+    puts "error"
+   end
+  end
+end
+
+
+# インスタンスnew
+run_command = RunCommand.new
+
+# IPアドレス入力画面処理, コマンド実行 
+run_command.addipaddr
+
+
+
